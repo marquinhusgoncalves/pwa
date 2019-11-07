9043be4af516b7d2d50420d9fb7c7ba77b4cd9ac
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2019-11-06 22:56:14 -0300

    chore: register service worker

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', function(event) {
+    console.log('[Service worker] Install Service Worker...', event)
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('[Service worker] Activating Service Worker...', event);
+    return self.clients.claim();
+});
